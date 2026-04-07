208962ff9de28c92f3a9ee19a65bc1735b2145a9
Stefan-Valentijn <stef.valentijn@gmail.com>
2026-04-07 20:07:28 +0200

    update analysis script

--- src/7.Analysis.R
+++ src/7.Analysis.R
@@ -1,20 +1,26 @@
 # Load libraries
 library(tidyverse)
 
-#Load dataset
-df <- read_csv("../data/new_cinema_streaming_data.csv")
+# Load dataset
+df <- read_csv("../data/cinema_streaming_data.csv")
 
-#Remove missign cases for now
-df <- df %>%
-  filter(!is.na(title))
+# Impression dataset
+summary(df)
+colSums(is.na(df))
 
-#Mean-center the IV and moderator before creating the interaction
+# Mean-center the IV and moderator before creating the interaction
 df <- df %>%
   mutate(
     domestic_gross_c  = scale(domestic_gross,  center = TRUE, scale = FALSE),
     release_window_c  = scale(release_window,  center = TRUE, scale = FALSE)
   )
 
-#Run the model
+# Estimates in model printed instead of scientific notation
+options(scipen = 999)
+
+# Run the model
 model <- lm(viewing_30days ~ domestic_gross_c * release_window_c + runtimeMinutes + COVID + IMDb_rating + 
-            production_budget, data = df); summary(model)
+            production_budget, data = df); summary(model); 
+
+# Cleaner results
+round(summary(model)$coefficients, 3)
