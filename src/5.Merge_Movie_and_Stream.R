69fda1b6aea8d443f6f75012dc9ae3bab0308be2
Stefan-Valentijn <stef.valentijn@gmail.com>
2026-05-08 13:48:36 +0200

    updates

--- src/5.Merge_Movie_and_Stream.R
+++ src/5.Merge_Movie_and_Stream.R
@@ -5,12 +5,20 @@ library(tidyverse)
 cinema <- read_csv("../data/movie_data.csv")          # resulting from script 3
 streaming <- read_csv("../data/streaming_data.csv")   # resulting from script 4
 
+###########
+# MERGING #
+###########
+
 # Merge based on tconst
 cinema_streaming_data <- streaming |> 
   left_join(cinema, by = "tconst")
 
 # Inventory management
 rm(cinema, streaming)
+
+#######################
+# FEATURE ENGINEERING #
+#######################
 
 # Feature engineer release window
 cinema_streaming_data <- cinema_streaming_data %>%
@@ -19,14 +27,6 @@ cinema_streaming_data <- cinema_streaming_data %>%
     release_streaming = as.Date(release_streaming, format = "%d-%m-%Y"),
     release_window    = as.numeric(difftime(release_streaming, release_cinema, units = "days"))
   )
-
-# Remove instances with negative release windows. that is not possible
-cinema_streaming_data <- cinema_streaming_data %>%
-  filter(release_window >= 0)
-
-# Add COVID variable
-cinema_streaming_data <- cinema_streaming_data %>%
-  mutate(COVID = ifelse(releaseYear %in% c(2020, 2021), 1, 0))
 
 # Reorder dataset for clarity
 cinema_streaming_data <- cinema_streaming_data %>%
@@ -39,47 +39,36 @@ cinema_streaming_data <- cinema_streaming_data %>%
     release_streaming,
     release_window,
     viewing_30days,
-    viewing_60days,
-    viewing_90days,
     streaming_platform,
     production_budget,
     domestic_gross,
     worldwide_gross,
     runtimeMinutes,
     genres,
+    blockbuster_score,
     IMDb_rating,
     IMDb_votecount,
-    COVID
+    COVID,
+    competition
   )
 
-# Blockbuster score as a proxy of incorporating genres
-genre_scores <- cinema_streaming_data %>%
-  select(genres, worldwide_gross) %>%
-  mutate(genre = strsplit(genres, ",")) %>%
-  unnest(genre) %>%
-  mutate(genre = trimws(genre)) %>%
-  group_by(genre) %>%
-  summarise(mean_bo = mean(worldwide_gross, na.rm = TRUE)) %>%
-  arrange(desc(mean_bo))
-
-# Then normalise
-genre_weights <- genre_scores %>%
-  mutate(score = (mean_bo - min(mean_bo)) / (max(mean_bo) - min(mean_bo))) %>%
-  select(genre, score) %>%
-  deframe()
-
+# There are some releases with negative release windows. that is not possible
 cinema_streaming_data <- cinema_streaming_data %>%
-  mutate(blockbuster_score = sapply(genres, function(g) {
-    gs <- trimws(strsplit(g, ",")[[1]])
-    mean(genre_weights[gs], na.rm = TRUE)  # average across genres per film
-  }))
+  filter(release_window >= 0)
+
+# Release windows that comprise years are not a strategic consideration and thus removed
+filtered_set <- cinema_streaming_data %>%
+  filter(release_window <= 365)
+
+
+
+
+
+
+
 
 # Impression dataset
-summary(cinema_streaming_data)
 colSums(is.na(cinema_streaming_data)) #no missing values
-
-# Inventory mangement
-rm(genre_scores, genre_weights)
 
 # Save dataset
 write.csv(cinema_streaming_data, "../data/cinema_streaming_data.csv", row.names = FALSE)
